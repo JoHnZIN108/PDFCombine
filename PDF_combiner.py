a29007acd7a29a40c6469bf1b1ab808d8e656154
JoHnZIN108 <amahjohnson2@gmail.com>
2024-08-11 00:26:47 -0400

    actual code

---
+++ PDF_combiner.py
@@ -0,0 +1,13 @@
+from pytube import YouTube
+from sys import argv
+import PyPDF2
+import sys
+import os
+
+merger = PyPDF2.PdfMerger() # To create merger object with the PyPDF class
+
+for file in os.listdir():
+    if file.endswith('.pdf'):
+        merger.append(file)
+        
+merger.write('Combined_pdf.pdf')
